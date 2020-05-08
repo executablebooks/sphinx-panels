9ea4e4d70154677d0f6ee8cd1eb9984875470c20
Chris Sewell <chrisj_sewell@hotmail.com>
2020-05-08 18:35:36 +0100

    Change panel delimiter and configuration

--- tests/test_panels.py
+++ tests/test_panels.py
@@ -7,15 +7,15 @@ from sphinx_panels import parse_panels
     "content,expected",
     (
         ("a", [{"body": (["a"], 0)}]),
-        ("...\na", [{"body": (["a"], 1)}]),
+        ("---\na", [{"body": (["a"], 1)}]),
         ("a\n^^^", [{"body": ([], 2), "header": (["a"], 0)}]),
         ("a\n+++", [{"body": (["a"], 0), "footer": ([], 1)}]),
         (
             "a\n^^^\nb\n+++\nc",
             [{"body": (["b"], 2), "footer": (["c"], 3), "header": (["a"], 0)}],
         ),
-        ("...\ncard = a", [{"body": ([], 2), "classes": {"card": ["a"]}}]),
-        ("a\n...\nb", [{"body": (["a"], 0)}, {"body": (["b"], 2)}]),
+        ("---\n:card: a", [{"body": ([], 2), "classes": {"card": ["a"]}}]),
+        ("a\n---\nb", [{"body": (["a"], 0)}, {"body": (["b"], 2)}]),
     ),
 )
 def test_parse_panels(content, expected):
