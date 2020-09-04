bcdbb2ff10896a2ec5868376b96aa8e37b94338a
Chris Sewell <chrisj_sewell@hotmail.com>
2020-09-04 20:58:09 +0100

    👌 IMPROVE: Move from CSS to SCSS (#31)

--- sphinx_panels/__init__.py
+++ sphinx_panels/__init__.py
@@ -1,8 +1,18 @@
 """"A sphinx extension to add a ``panels`` directive."""
-import os
+import hashlib
+from pathlib import Path
+import warnings
 
 from docutils import nodes
 from docutils.parsers.rst import directives, Directive
+from sphinx.application import Sphinx
+from sphinx.environment import BuildEnvironment
+from sphinx.util.logging import getLogger
+
+with warnings.catch_warnings():
+    # TODO this can re removed when pyScss 1.3.8 is released
+    warnings.simplefilter(action="ignore", category=FutureWarning)
+    from scss.compiler import compile_file
 
 from .button import setup_link_button
 from .dropdown import setup_dropdown
@@ -11,15 +21,51 @@ from .icons import setup_icons
 
 __version__ = "0.4.1"
 
+LOGGER = getLogger(__name__)
 
-LOCAL_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
+def compile_scss(app: Sphinx):
+    """Compile SCSS to the build directory."""
+    # reset css changed attribute
+    app.env.panels_css_changed = False
 
-def add_static_paths(app):
-    app.config.html_static_path.append(os.path.join(LOCAL_FOLDER, "css"))
-    app.add_css_file("sphinx-dropdown.css")
+    # setup up new static path in output dir
+    static_path = (Path(app.outdir) / "_panels_static").absolute()
+    static_path.mkdir(exist_ok=True)
+    app.config.html_static_path.append(str(static_path))
+
+    # set sass files to compile
+    sass_path = Path(__file__).parent.joinpath("scss").absolute()
+    sass_files = {"sphinx-panels-dropdown": sass_path / "dropdown" / "index.scss"}
     if app.config.panels_add_boostrap_css:
-        app.add_css_file("panels-bootstrap.min.css")
+        sass_files["sphinx-panels-bootstrap"] = sass_path / "bootstrap" / "index.scss"
+
+    for name, path in sass_files.items():
+        # get the string and hash of the css
+        css_string_out = compile_file(str(path), output_style="compressed")
+        hashstring = hashlib.md5(css_string_out.encode("utf8")).hexdigest()
+        css_name = f"{name}.{hashstring}.css"
+        if not (static_path / css_name).exists():
+            # remove any old hashes for re-builds
+            for old_path in Path(app.outdir).glob(f"**/{name}.*.css"):
+                old_path.unlink()
+            # write the file
+            (static_path / css_name).write_text(css_string_out, encoding="utf8")
+            # note that css has changed
+            app.env.panels_css_changed = True
+        app.add_css_file(css_name)
+
+
+def update_css_links(app: Sphinx, env: BuildEnvironment):
+    """If CSS has changed, all files must be re-written,
+    to include the correct stylesheets.
+    """
+    # note, ideally here we would only do this for html builders
+    # but this is actually quite hard to identify, since the builder name doesn't
+    # always include 'html' (e.g. readthedocs)
+    if env.panels_css_changed:
+        LOGGER.debug("sphinx-panels CSS changed; re-writing all files")
+        return list(env.all_docs.keys())
 
 
 class Div(Directive):
@@ -53,7 +99,7 @@ class Div(Directive):
         return [node]
 
 
-def visit_container(self, node):
+def visit_container(self, node: nodes.Node):
     classes = "docutils container"
     if node.get("is_div", False):
         # we don't want the CSS for container for these nodes
@@ -61,14 +107,15 @@ def visit_container(self, node):
     self.body.append(self.starttag(node, "div", CLASS=classes))
 
 
-def depart_container(self, node):
+def depart_container(self, node: nodes.Node):
     self.body.append("</div>\n")
 
 
-def setup(app):
+def setup(app: Sphinx):
     app.add_directive("div", Div)
     app.add_config_value("panels_add_boostrap_css", True, "env")
-    app.connect("builder-inited", add_static_paths)
+    app.connect("builder-inited", compile_scss)
+    app.connect("env-updated", update_css_links)
     # we override container html visitors, to stop the default behaviour
     # of adding the `container` class to all nodes.container
     app.add_node(
