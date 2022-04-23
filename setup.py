7c26c838ce5e6a097e580e402361795793458171
Chris Holdgraf <choldgraf@berkeley.edu>
2022-04-23 08:34:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     extras_require={
         "themes": [
             # ref: https://github.com/sphinx-doc/sphinx/issues/10291
-            "jinja<3.1",
+            "Jinja2<3.1",
             "sphinx-rtd-theme",
             "pydata-sphinx-theme~=0.4.0",
             "sphinx-book-theme~=0.0.36",
